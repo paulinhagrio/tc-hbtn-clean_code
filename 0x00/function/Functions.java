12bd80a93c5c40afc5bc803d151bb890c5d835df
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-07-04 11:53:19 -0300

    Funçoes

--- 0x00/function/Functions.java
+++ 0x00/function/Functions.java
@@ -10,12 +10,12 @@ public class Functions {
     }
 
     //Somente 1 nível de abstração
-
-    //B
+    //A
     @PostMapping("/user")
     public User saveUser(User user){
-        return userService.userRepository.save(user);
+        return userService.save(user);
     }
+
 
     // Arguments
     //A
